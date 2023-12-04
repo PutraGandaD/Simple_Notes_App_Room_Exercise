6586cf95974d95f0ea74bf2d86f20dce5a2017c6
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2023-12-04 10:57:22 +0700

    Added Add/Edit Notes Layout

---
+++ app/src/main/java/com/putragandad/roomexercise/activities/AddEditNoteActivity.kt
@@ -0,0 +1,12 @@
+package com.putragandad.roomexercise.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.putragandad.roomexercise.R
+
+class AddEditNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_edit_note)
+    }
+}
